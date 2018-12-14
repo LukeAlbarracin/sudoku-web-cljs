5e15d17804b927602b1a6f121accda53ce327e72
lukealba <lalbarracin2000@gmail.com>
2018-12-14 13:57:18 -0600

    Small change

--- src/game_sudoku/core.clj
+++ src/game_sudoku/core.clj
@@ -1,35 +1,28 @@
 (ns game-sudoku.core
   (:gen-class))
 
-(defn gen-grid []
- "(flatten (nth (nth ... ?) 0..2)) -> square-x"
- [[[5 3 0] [0 7 0] [0 0 0]]
-  [[6 0 0] [1 9 5] [0 0 0]]
-  [[0 9 8] [0 0 0] [0 6 0]]
-
-  [[8 0 0] [0 6 0] [0 0 3]]
-  [[4 0 0] [8 0 3] [0 0 1]]
-  [[7 0 0] [0 2 0] [0 0 6]]
-
-  [[0 6 0] [0 0 0] [2 8 0]]
-  [[0 0 0] [4 1 9] [0 0 5]]
-  [[0 0 0] [0 8 0] [0 7 9]]])
-
-(defn gen-grids []
-  [5 3 0 0 7 0 0 0 0
-   6 0 0 1 9 5 0 0 0
-   0 9 8 0 0 0 0 6 0
-   8 0 0 0 6 0 0 0 3
-   4 0 0 8 0 3 0 0 1
-   7 0 0 0 2 0 0 0 6
-   0 6 0 0 0 0 2 8 0
-   0 0 0 4 1 9 0 0 5
-   0 0 0 4 1 9 0 0 5])
+(def sudoku-grid 
+  (atom
+    [5 3 0 0 7 0 0 0 0
+     6 0 0 1 9 5 0 0 0
+     0 9 8 0 0 0 0 6 0
+     8 0 0 0 6 0 0 0 3
+     4 0 0 8 0 3 0 0 1
+     7 0 0 0 2 0 0 0 6
+     0 6 0 0 0 0 2 8 0
+     0 0 0 4 1 9 0 0 5
+     0 0 0 4 1 9 0 0 5]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (partition 9 (flatten (flatten (gen-grid))))))
+  (println (deref sudoku-grid))
 
-;;(defn check-horizontal [square-x square-y grid-x grid-y]
+(defn horizontal? [grid testval y]]
+  (contains? (nth (partition 9 grid) y) testval))
+
+(defn vertical? [grid testval x]]
+  (contains? (nth (apply mapv vector (partition 9 grid)) x) testval))
+  
+;;(defn square-test? [grid testval xx yy]
   
