179f4a4705e0647529cebb85e77b3a96dafcf403
lukealba <lalbarracin2000@gmail.com>
2019-01-09 18:19:12 -0600

    Changes.

--- src/game_sudoku/core.clj
+++ src/game_sudoku/core.clj
@@ -5,18 +5,17 @@
   (atom
     [5 3 0 0 7 0 0 0 0
      6 0 0 1 9 5 0 0 0
-     0 9 8 0 0 0 0 6 0
      8 0 0 0 6 0 0 0 3
      4 0 0 8 0 3 0 0 1
      7 0 0 0 2 0 0 0 6
      0 6 0 0 0 0 2 8 0
      0 0 0 4 1 9 0 0 5
-     0 0 0 4 1 9 0 0 5]))
+     0 0 0 0 8 0 0 7 9]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (deref sudoku-grid))
+  (println (deref sudoku-grid)))
 
 (defn horizontal? [grid testval y]]
   (contains? (nth (partition 9 grid) y) testval))
@@ -24,5 +23,14 @@
 (defn vertical? [grid testval x]]
   (contains? (nth (apply mapv vector (partition 9 grid)) x) testval))
   
-;;(defn square-test? [grid testval xx yy]
+(defn square? [grid testval sq-coordinate]
+  (->> grid
+      (partition 9)
+      (map #(partition 3 %)))
+      (apply mapv vector)
+      (flatten)
+      (partition 9))
+
+
+
   
